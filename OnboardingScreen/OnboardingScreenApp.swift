e7943519b0b290cba5fe7d4b7f79cb30e3777530
Appslix <mr.aadouli@gmail.com>
2021-12-27 04:56:08 +0100

    Initial Commit

---
+++ OnboardingScreen/OnboardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingScreenApp.swift
+//  OnboardingScreen
+//
+//  Created by Yassine AADOULI on 27/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
